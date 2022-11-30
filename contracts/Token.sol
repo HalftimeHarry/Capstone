dba0fd4f49bf07ff69d0c59cf14678343ff4313f
Dustin L Dinsmore <ddinsmore8@gmail.com>
2022-11-30 01:12:21 +0000

    Token Transfer

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "The Blizzard of Odds";
-    string public symbol = "BLZ";
+    string public name;
+    string public symbol;
     uint256 public decimals = 18;
-    uint256 public totalSupply = 1000000 * (10 ** decimals);
+    uint256 public totalSupply;
 
-    // Track Balances
-    mapping (address => uint256) public balanceOf;
-    // Send Tokens
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
     constructor(
         string memory _name,
@@ -22,5 +22,20 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * (10 ** decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
